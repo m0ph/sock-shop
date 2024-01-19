21fc91f47735fddea89a61ee7cecf236b4e23bd3
m0ph <morris.peichl@gmx.de>
2024-01-19 14:02:09 +0100

    tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,19 @@
+locals {
+  name   = "sock-shop-cluster"
+  region = "eu-west-3"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = ["eu-west-3a", "eu-west-3b"]
+
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
+
+  tags = {
+    Example = local.name
+  }
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 4.0"
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,18 +2,3 @@ provider "aws" {
   region = local.region
 }
 
-locals {
-  name   = "sock-shop-cluster"
-  region = "eu-west-3"
-
-  vpc_cidr = "10.0.0.0/16"
-  azs      = ["eu-west-3a", "eu-west-3b"]
-
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
-
-  tags = {
-    Example = local.name
-  }
-}
