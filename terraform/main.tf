8b7bd83c03c2eb3d81ceff345233659500458034
m0ph <morris.peichl@gmx.de>
2024-01-22 13:49:24 +0100

    no kms

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,6 +60,7 @@ module "eks" {
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
+  create_kms_key                 = false
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
