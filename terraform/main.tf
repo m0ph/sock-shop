df23dfff288236485cc4329121cd96068b774950
m0ph <morris.peichl@gmx.de>
2024-01-22 17:30:11 +0100

    comments

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,7 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-
+# set provider to Amazon Web Services
+# region is defined in variables.tf
 provider "aws" {
   region = var.region
 }
@@ -13,28 +14,32 @@ data "aws_availability_zones" "available" {
     values = ["opt-in-not-required"]
   }
 }
-
+# local variables
+# define clustername and append random suffix via "random_string" ressource
 locals {
   cluster_name = "sock-shop-${random_string.suffix.result}"
 }
 
+# random string for clustername
 resource "random_string" "suffix" {
   length  = 8
   special = false
 }
 
+# define the VPC
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
-
+# name of the VPC
   name = "sock-shop-vpc"
-
+# IP block of the VPC
   cidr = "10.0.0.0/16"
+# Availability Zones used (all from 0-3)
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
-
+# subnets
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-
+# gateways
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
@@ -49,22 +54,26 @@ module "vpc" {
     "kubernetes.io/role/internal-elb"             = 1
   }
 }
-
+# define EKS cluster
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.3"
-
+# cluster name
   cluster_name    = local.cluster_name
   cluster_version = "1.27"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
+  # make control plane publicly abaliable
   cluster_endpoint_public_access = true
+  # no key managed by kms (permission issue)
   create_kms_key                 = false
   create_cloudwatch_log_group    = false
+  # disable encyption so we dont need a key
   cluster_encryption_config      = {}
 
+# deprecated / obsolete
   cluster_addons = {
     coredns = {
       most_recent = true
@@ -76,7 +85,7 @@ module "eks" {
       most_recent = true
     }
   }
-
+# define managed node group
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
   }
@@ -94,7 +103,7 @@ module "eks" {
     }
   }
 }
-
+# ebs csi runs into timeout
 /*
 # https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
 data "aws_iam_policy" "ebs_csi_policy" {
