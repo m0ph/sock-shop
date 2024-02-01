b98b8f5b5b4638cd7f16c5780615f4b97bd3b7e4
m0ph <morris.peichl@gmx.de>
2024-02-01 10:45:03 +0100

    aws lb and role/policy

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,9 @@ variable "region" {
   type        = string
   default     = "eu-west-3"
 }
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type = string
+  default = "sock-shop-project"
+}
---
+++ terraform/vpc.tf
@@ -0,0 +1,44 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+# set provider to Amazon Web Services
+# region is defined in variables.tf
+provider "aws" {
+  region = var.region
+}
+
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+# define the VPC
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+# name of the VPC
+  name = "sock-shop-vpc"
+# IP block of the VPC
+  cidr = "10.0.0.0/16"
+# Availability Zones used (all from 0-3)
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+# subnets
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+# gateways
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
